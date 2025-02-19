10e9a12ae7e9e9ce9ea86c04d52db749331ddbde
nick282005 <rafaeldesousacavalcante21@gmail.com>
2025-02-19 09:56:45 -0300

    java q eu e gui fez

---
+++ eclipse-workspace/aula-2/src/EntradaDeDados.java
@@ -0,0 +1,49 @@
+
+import java.util.Scanner;
+
+public class EntradaDeDados {
+	    public static void main(String[] arg) 
+	    {
+	    	Scanner sc =  new Scanner(System.in);
+	    	
+	    	String n;
+	    	
+	    	System.out.println ("Qual o usuario");
+	    	
+	    	n = sc.next();
+	    	
+	    	System.out.println (n);
+	    	
+	    	System.out.println ("Qual a sua idade");
+	    	int i = sc.nextInt();
+	    	System.out.println ("Sua idade é " + i);
+	    	
+	    	System.out.println("Qual sua altura: ");
+	    	double a = sc.nextDouble ();
+	    	System.out.println("Sua Altura é" + a );
+	    	
+	    	
+	    	if (i == 19) {
+	    		System.out.println ("Logado Bem-vindo" + n + i + a);
+				
+			}
+	    	else {
+	    		System.out.println ("Barrado" + n);
+			}
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	sc.close();
+	    }
+	}
+	
+
+
+
---
+++ eclipse-workspace/aula-2/src/Teste.java
@@ -0,0 +1,29 @@
+
+public class Teste {
+	
+	public static void main (String[] args) {
+		System.out.println ("Pronto Agora Vai");
+		
+		var NomeCompleto = "gui";
+		int N1 = 5;
+		int N2 = 2;
+		var cal = N1+N2;
+		float Peso = 70.6f;
+		
+		System.out.println (cal);
+		System.out.println(Peso);
+		System.out.println (NomeCompleto);
+		
+		
+		if (cal == 10) {
+			System.out.println ("Aprovado");
+			
+		}
+		else {
+			System.out.println ("Moio fi");
+			
+		}
+	}
+}
+
+
