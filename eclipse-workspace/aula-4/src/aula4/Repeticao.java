a52993814764a1085eeab75843aa59b1b1428f01
rafaelvon1 <rafaeldesousacavalcante21@gmail.com>
2025-03-12 08:56:50 -0300

    jogo completo

--- eclipse-workspace/aula-4/src/aula4/Repeticao.java
+++ eclipse-workspace/aula-4/src/aula4/Repeticao.java
@@ -1,5 +1,34 @@
 package aula4;
 
-public class Repeticao {
+import java.util.Scanner;
 
+public class Repeticao {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int valor;
+		int aleatorio = (int) (Math.random() * 10);
+		
+		do {
+			System.out.println("que numero estou pensando??---");
+			valor = sc.nextInt();
+			if (aleatorio > valor) {
+				System.out.println("O numero que pensei é maior que " + valor);
+				
+			}if (aleatorio < valor) {
+				System.out.println("O numero que pensei é menor que " + valor);
+			}
+		} while (aleatorio != valor);
+		
+		System.out.println("Congratulation");
+		
+		
+		//System.out.println(aleatorio);
+		//System.out.println(valor);
+		
+		sc.close();
+	}
 }
